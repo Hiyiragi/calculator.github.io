fdf50dd354d7aa09a93abb8d833ef34b1fdbb2ae
Hiyiragi <immerarsenal@gmail.com>
2023-03-22 21:37:39 +0100

    improved structure and added js

---
+++ javascript.js
@@ -0,0 +1,19 @@
+let bodyElement = document.querySelector("body");
+let textArea = document.querySelector(".textArea input");
+let inButtons = document.querySelectorAll("button.in");
+let funcButtons = document.querySelectorAll("button.func");
+
+for (const inButton of inButtons) {
+    inButton.addEventListener("click", function(){
+        let val = this.getAttribute("data-value")
+        textArea.value += val;
+    });
+    
+}
+
+for (const funcButton of funcButtons) {
+    funcButton.addEventListener("click", function(){
+        let val = this.getAttribute("data-value")
+    });
+
+}
